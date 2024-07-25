207ed72a58de889fd685982282b7c92f4003ed15
Suryansh Sharma <suryansharma09@gmail.com>
2024-07-25 18:15:05 +0530

    added databse model

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'               #session management, security features
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url_db.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 
 db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
