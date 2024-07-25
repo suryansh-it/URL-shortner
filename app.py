af4fdfe9c55b54c48c06312b70844cf42db0391d
Suryansh Sharma <suryansharma09@gmail.com>
2024-07-25 19:02:12 +0530

    LOGIC COMPLETE WITH COMMENTS

--- app.py
+++ app.py
@@ -19,9 +19,14 @@ def generate_short_url():                                 #generate random strin
     short_url = ''.join(random.choice(characters) for _ in range(6))        #joins the list of characters into a single string, which forms the short URL.
     link = URL.query.filter_by(short_url=short_url).first()                     # queries the URL table for any records with the short_url equal to the newly generated one
                                                                                 # and returns the first result
-    if link:                                                                    
-        return generate_short_url()
-    return short_url
+    if link:                                                                #if true , short url exists in db , hence created url is not unique 
+        return generate_short_url()                                 #function calls itself recursively to generate a new short_url
+                                                                    #until a unique short_url is found
+
+    return short_url        #If no existing link is found (i.e., the short_url is unique), the function returns the short_url.
+
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from app import db
 class URL():                                    #database model for URL
     __tablename__ = 'url'
     id = db.Column(db.Integer, primary_key=True)
-    og_url = db.Column(db.String(200), nullable=False)
-    short_url = db.Column(db.String(10), nullable=False, unique=True)
-    date_creation = db.Columnn(db.DateTime, default=db.func.now())
+    og_url = db.Column(db.String(200), nullable=False)              #CANT BE NULL
+    short_url = db.Column(db.String(10), nullable=False, unique=True)   #STORED URL SHOULD BE UNIQUE
+    date_creation = db.Columnn(db.DateTime, default=db.func.now())          
 
 
 
