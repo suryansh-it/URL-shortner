78d4029d5c4f34d752576bcff8797ea525553498
Suryansh Sharma <suryansharma09@gmail.com>
2024-07-27 19:57:59 +0530

    final commit , bugs resolved ,app running

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
 from flask import Flask , render_template , request , redirect , url_for ,flash
-import string , random
 from flask_sqlalchemy import SQLAlchemy
-from models import URL
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'               #session management, security features
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url_db.db'
+import string , random
+from models import db,URL  # Imported models here to avoid circular imports
 
 
 
-db = SQLAlchemy(app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secretkey'        #session management, security features
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///url_db.db'
+    
+
+    db.init_app(app)            #line links the SQLAlchemy instance (db) with the Flask app (app)
+
+    with app.app_context():             #Ensure that database operations like db.create_all()
+                                        #are performed within an application context.
+       
+        db.create_all()  # Creating tables based on defined models
+
+    app_routes(app)
+    return app 
+
+
+
 
 
 #logic for url shortner
 
 def generate_short_url():                                 #generate random string for short url
+      
     characters = string.ascii_letters + string.digits           #includes all alphanumeric chars
     short_url = ''.join(random.choice(characters) for _ in range(6))        #joins the list of characters into a single string, which forms the short URL.
     link = URL.query.filter_by(short_url=short_url).first()                     # queries the URL table for any records with the short_url equal to the newly generated one
@@ -27,37 +42,41 @@ def generate_short_url():                                 #generate random strin
 
 
 
-#route for index , generating shortend url
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        og_url = request.form['original_url']
-        short_url = generate_short_url()
-        new_url = URL(og_url= og_url ,short_url = short_url)
-
-        db.session.add(new_url)                 #This line adds the new URL instance to the current database session.
-                                                #This operation prepares the object to be written to the database
-
-        db.session.commit()                     # commits the current transaction to the database
-
-        return render_template('result.html' , short_url= short_url)
-    return render_template('index.html')
 
 
-#mapping a shortened URL to the original, full UR
+def app_routes(app):
+    #route for index , generating shortend url
+
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        if request.method == 'POST':
+            og_url = request.form['original_url']
+            short_url = generate_short_url()
+            new_url = URL(og_url= og_url ,short_url = short_url)
+
+            db.session.add(new_url)                 #This line adds the new URL instance to the current database session.
+                                                    #This operation prepares the object to be written to the database
+
+            db.session.commit()                     # commits the current transaction to the database
+
+            return render_template('result.html' , short_url= short_url)
+        return render_template('index.html')
 
 
-app.route("/<short_url>")
-def redirect_to_url(short_url):
-    link = URL.query.filter_by(short_url=short_url)             #function queries the database to find the entry in the URL table
-                                                                #where the short_url column matches the provided short_url
+    #mapping a shortened URL to the original, full UR
 
-    return redirect(link.og_url)            #if a matching short_url is found in the db, the func retrieves
-                                            #the corresponding original_url from the link object.
+
+    @app.route("/<short_url>")
+    def redirect_to_url(short_url):
+        link = URL.query.filter_by(short_url=short_url).first_or_404()             #function queries the database to find the entry in the URL table
+                                                                    #where the short_url column matches the provided short_url
+
+        return redirect(link.og_url)            #if a matching short_url is found in the db, the func retrieves
+                                                #the corresponding original_url from the link object.
 
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = create_app()
+    app.run(debug=True, port =5001)
--- models.py
+++ models.py
@@ -1,11 +1,15 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 
-class URL():                                    #database model for URL
-    __tablename__ = 'url'
+db = SQLAlchemy()
+
+class URL(db.Model):                                    #database model for URL
+    __tablename__ = 'urls'
     id = db.Column(db.Integer, primary_key=True)
     og_url = db.Column(db.String(200), nullable=False)              #CANT BE NULL
     short_url = db.Column(db.String(10), nullable=False, unique=True)   #STORED URL SHOULD BE UNIQUE
-    date_creation = db.Columnn(db.DateTime, default=db.func.now())          
+    date_creation = db.Column(db.DateTime, default=db.func.now())          
+
+    
 
 
 
